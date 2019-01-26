627d0a748574ff1f17d65df955ca53a9c444261a
Abhishek <u6411540@anu.edu.au>
2019-01-26 17:47:32 +1100

    q

--- 3_counter-app/src/components/counter.jsx
+++ 3_counter-app/src/components/counter.jsx
@@ -7,11 +7,15 @@ import React , {Component} from 'react';
      render(){
          return(
         <div>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span class={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button class  = "btn btn-secondary btn-sm">Increment</button>
         </div>
          );
      } 
+
+     getBadgeClasses(){
+         return(this.state.count==0?"badge badge-warning":"btn btn-primary btn-sm" )
+     }
 
     formatCount(){
       return(this.state.count==0?'Zero':this.state.count);
