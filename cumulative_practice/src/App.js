43bc394b2e8d7de810f5261f6aab5b906bd4daac
Abhishek <u6411540@anu.edu.au>
2019-01-28 21:31:26 +1100

    We made a mistake so reverted back to an earlier commit

--- cumulative_practice/src/App.js
+++ cumulative_practice/src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+<<<<<<< HEAD
 import Counters from './components/counters';
 import Counter from './components/counter';
+=======
+>>>>>>> parent of ae374a6... Change : Earlier counter was imported in index .js , now used app.js to import it
 
 class App extends Component {
 
@@ -10,6 +13,7 @@ state = {
   
 }
   render() {
+<<<<<<< HEAD
 
     return (  
      
@@ -19,6 +23,27 @@ state = {
       );
     
   }  
+=======
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+>>>>>>> parent of ae374a6... Change : Earlier counter was imported in index .js , now used app.js to import it
 }
 
 export default App;
--- cumulative_practice/src/index.js
+++ cumulative_practice/src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import Counter from "./components/counter";
 import "bootstrap/dist/css/bootstrap.css"
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<Counter/>, document.getElementById('root'));
 
 
 serviceWorker.unregister();
