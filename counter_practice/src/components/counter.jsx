3b9ee72211a5bc81cd6fb1fe6d9d35ff9cd7208f
Abhishek <u6411540@anu.edu.au>
2019-02-07 19:35:00 +1100

    A full function counter with a RESET button has been created. Further this counter implements the SINGLE SOURCE OF TRUTH

--- counter_practice/src/components/counter.jsx
+++ counter_practice/src/components/counter.jsx
@@ -4,27 +4,22 @@ import React , {Component} from "react";
 class Counter extends Component{
   
 
-    state = {
-        value : 1 ,
-    }
 
-    handleIncrement=()=>{
-         this.setState({value:this.state.value+1})
-        }
+
  
   
 
     render(){
         return( 
             <div>
-                <span class="badge badge-info">{this.state.value}</span>
-                <button onClick = {this.handleIncrement} type="button" class="btn btn-secondary">Increment</button>
+                <span class="badge badge-info">{this.props.counter.value}</span>
+                <button onClick = {()=>this.props.handleIncrement()} type="button" class="btn btn-secondary">Increment</button>
                 <button onClick = {this.props.handleDelete} type="button" class="btn btn-danger">Delete</button>
             </div>
     );
        
     }
 }
-
+ 
 export default Counter;
 
--- counter_practice/src/components/counters.jsx
+++ counter_practice/src/components/counters.jsx
@@ -11,6 +11,22 @@ class Counters extends Component{
             {id :4 ,value : 4 }
         ],
 }
+    handleReset=()=>{
+         
+    const temp_counters = this.state.counters.map((temp)=>{
+        temp.value = 0; 
+        return temp});
+        this.setState({counters:temp_counters});
+    }
+    Increment_the_counter=(counter)=>{
+        const counter_updated = this.state.counters.map((temp)=>{
+                if(counter.id ===temp.id){
+                        counter.value++;
+                }
+            })
+            this.setState(counter_updated);
+        
+    }
 
   
     deleteCounter(counter_id){
@@ -23,9 +39,13 @@ class Counters extends Component{
     
         return(
             <div>
+                <button onClick = {this.handleReset} type="button" class="btn btn-primary btn-lg">RESET</button>
                 {this.state.counters.map((temp_counter)=>(
-                <Counter key ={temp_counter.id}  id = {temp_counter.id} 
-                value = {temp_counter.value} handleDelete ={()=>{this.deleteCounter(temp_counter.id)}} ></Counter>
+                <Counter key ={temp_counter.id} 
+                handleIncrement ={()=>this.Increment_the_counter(temp_counter)} 
+                handleDelete ={()=>{this.deleteCounter(temp_counter.id)}}
+                counter = {temp_counter} ></Counter>
+               
                 ))}
             </div>
 
