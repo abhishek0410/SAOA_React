2076f5da9b70ee0a9f942cbe9318b3dc21806c89
Abhishek <u6411540@anu.edu.au>
2019-02-20 17:24:22 +1100

    A functional ListGroup has been added . SO the webpage now has pagination , Listgroup but no sorting . We will add the sorting sometimes later now back to building the form

--- vidly_copy/src/components/movies.jsx
+++ vidly_copy/src/components/movies.jsx
@@ -1,8 +1,10 @@
 import React , {Component} from "react";
 import {getMovies} from "../services/fakeMovieService";
-import Like from './like';
 import Pagination from './pagination';
 import Paginate from "../utils/paginate";
+import ListGroup from "./listGroup"
+import {getGenres} from "../services/fakeGenreService";
+import MoviesTable from "./moviesTable";
 class Movies extends Component {
     state = {
         movies : getMovies(),
@@ -10,13 +12,13 @@ class Movies extends Component {
         num_of_movies_per_page : 4,
         current_page : 1 //Default
     }
-    handleDelete=(movie)=>{
-        
+    handleDelete=(movie)=>{  
         const movie_update = this.state.movies.filter((temp_movie)=>(
             temp_movie._id!==movie._id
         )) 
         this.setState({movies:movie_update}) 
     }
+
 
     toggleLike=(movie)=>{
     console.log("Following movie clicked",movie.title," ");
@@ -24,53 +26,49 @@ class Movies extends Component {
      const movie_update = this.state.movies.map((temp_movie)=>{
             if(movie._id===temp_movie._id){
                 temp_movie.liked = !temp_movie.liked
-             //   console.log(temp_movie.liked)
-         }
-         
+         } 
         })
         this.setState({movie_update})
     }
     handlepageChange=(page_number)=>{
         this.setState({current_page:page_number})
     }
-    render(){
-        const {movies,num_of_movies_per_page,current_page} = this.state;
-        const movies_on_current_page = Paginate(movies,current_page,num_of_movies_per_page);
-        return(
-            <div> 
-            <table className ="table">
-                <thead>
-                    <tr>
-                        <th>Title</th>
-                        <th>Genre</th>
-                        <th>Stock</th>
-                        <th>DailyRental</th>
-                        <th></th>
-                        <th></th>
-                    </tr>
-                </thead>
-                {movies_on_current_page.map((temp_movie)=>(
-                        <tbody key ={temp_movie._id} >
-                            <tr >
-                                <td>{temp_movie.title}</td>
-                                <td>{temp_movie.genre.name}</td>
-                                <td>{temp_movie.numberInStock}</td>
-                                <td>{temp_movie.dailyRentalRate}</td>
-                                <td><Like handleLike={()=>this.toggleLike(temp_movie)} liked_or_not = {temp_movie.liked}></Like></td>
-                                <td>
-                                    <button onClick={()=>this.handleDelete(temp_movie)}  type="button" className="btn btn-danger">Delete</button>  
-                                </td>
-            
-                            </tr>
-                         </tbody>
-                ))}
-            </table> 
-            <Pagination movies_count = {this.state.movies_count}
-            num_of_movies_per_page = {this.state.num_of_movies_per_page}
-            changePage = {this.handlepageChange}
-            >
 
-            </Pagination>
+    handleListGroup=(genre_name)=>{
+      // console.log(genre_name);
+        var movies_filtered = this.state.movies.filter((movies)=>(
+                movies.genre.name===genre_name.name 
+        ))
+        if(genre_name.name ==="All Genres"){
+            movies_filtered = getMovies();
+        }
+        this.setState({movies:movies_filtered ,current_page:1});
+        
+     }
+
+    render(){
+       const {movies,num_of_movies_per_page,current_page} = this.state;
+          return(
+            <div className ="row">
+                <div className ="col-4">
+                    <ListGroup movieGenre = {getGenres} 
+                        handleListGroup={this.handleListGroup}>
+                    </ListGroup>
+               </div>
+              
+                <div className ="col-1">
+                     <div>
+                         <MoviesTable movies={movies} num_of_movies_per_page={num_of_movies_per_page} 
+                            current_page={current_page} handleDelete={this.handleDelete}
+                            toggleLike={this.toggleLike} 
+                        />
+                        <Pagination movies_count = {this.state.movies_count}
+                            num_of_movies_per_page = {this.state.num_of_movies_per_page}
+                            changePage = {this.handlepageChange}>
+                        </Pagination>
+                     </div>
+                </div>
+
         </div>
   
         );
