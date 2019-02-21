99dea8ae20c380f474db0163b57e7d4df2ddb66a
Abhishek <u6411540@anu.edu.au>
2019-02-21 16:42:23 +1100

    A validation has been added which just says (does not display on screen however , that the password or the username field is empty . Next commit will use a java script library called the Joi-Browser and in there we will use display the message on the screen

--- section_6_routing/start/router-app/src/components/loginForm.jsx
+++ section_6_routing/start/router-app/src/components/loginForm.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
+import Joi from "joi-browser";
 import Input from "./input";
 
 class LoginForm extends Component {
   state = {
-    account: { username: "", password: "" }
+    account: { username: "", password: "" },
+    errors: {}
   };
+
   handleChange = e => {
     console.log("We are in the handleChange");
     const account = { ...this.state.account };
@@ -13,22 +16,35 @@ class LoginForm extends Component {
     this.setState({ account });
   };
 
+  validate = () => {
+    const errors = {};
+
+    if (this.state.account.username.trim() === "") {
+      errors.username = "Username is required";
+    }
+    if (this.state.account.password.trim() === "") {
+      errors.password = "Password is required";
+    }
+    return errors;
+  };
   handleSubmit = e => {
     e.preventDefault();
+    const errors = this.validate();
+    this.setState({ errors });
     console.log("Form submitted");
   };
-  handleLogin() {
-    console.log("Login button clicked");
-  }
+
   render() {
     return (
       <div>
         <h1>Login</h1>
-        <form onSubmit={this.handleSubmit} />
-        <Input handleChange={this.handleChange} account={this.state.account} />
-        <button onClick={this.handleLogin} className="btn btn-primary">
-          Login
-        </button>
+        <form onSubmit={this.handleSubmit}>
+          <Input
+            handleChange={this.handleChange}
+            account={this.state.account}
+          />
+          <button className="btn btn-primary">Login</button>
+        </form>
       </div>
     );
   }
