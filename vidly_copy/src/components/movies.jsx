d3cbbcbf44406422ba33855bac5df800bb8a8488
Abhishek <u6411540@anu.edu.au>
2019-02-24 14:31:56 +1100

    Ascending order sorting added

--- vidly_copy/src/components/movies.jsx
+++ vidly_copy/src/components/movies.jsx
@@ -1,82 +1,85 @@
-import React , {Component} from "react";
-import {getMovies} from "../services/fakeMovieService";
-import Pagination from './pagination';
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+import Pagination from "./pagination";
 import Paginate from "../utils/paginate";
-import ListGroup from "./listGroup"
-import {getGenres} from "../services/fakeGenreService";
+import ListGroup from "./listGroup";
+import { getGenres } from "../services/fakeGenreService";
 import MoviesTable from "./moviesTable";
+import _ from "lodash";
 class Movies extends Component {
-    state = {
-        movies : getMovies(),
-        movies_count : getMovies().length,
-        num_of_movies_per_page : 4,
-        current_page : 1 //Default
+  state = {
+    movies: getMovies(),
+    movies_count: getMovies().length,
+    num_of_movies_per_page: 4,
+    current_page: 1, //Default
+    sortColumn: []
+  };
+  handleDelete = movie => {
+    const movie_update = this.state.movies.filter(
+      temp_movie => temp_movie._id !== movie._id
+    );
+    this.setState({ movies: movie_update });
+  };
+
+  toggleLike = movie => {
+    console.log("Following movie clicked", movie.title, " ");
+
+    const movie_update = this.state.movies.map(temp_movie => {
+      if (movie._id === temp_movie._id) {
+        temp_movie.liked = !temp_movie.liked;
+      }
+    });
+    this.setState({ movie_update });
+  };
+  handlepageChange = page_number => {
+    this.setState({ current_page: page_number });
+  };
+
+  handleListGroup = genre_name => {
+    // console.log(genre_name);
+    var movies_filtered = this.state.movies.filter(
+      movies => movies.genre.name === genre_name.name
+    );
+    if (genre_name.name === "All Genres") {
+      movies_filtered = getMovies();
     }
-    handleDelete=(movie)=>{  
-        const movie_update = this.state.movies.filter((temp_movie)=>(
-            temp_movie._id!==movie._id
-        )) 
-        this.setState({movies:movie_update}) 
-    }
+    this.setState({ movies: movies_filtered, current_page: 1 });
+  };
+  handleonSort = path => {
+    this.setState({ sortColumn: { path: path, order: "asc" } });
+  };
 
-
-    toggleLike=(movie)=>{
-    console.log("Following movie clicked",movie.title," ");
-    
-     const movie_update = this.state.movies.map((temp_movie)=>{
-            if(movie._id===temp_movie._id){
-                temp_movie.liked = !temp_movie.liked
-         } 
-        })
-        this.setState({movie_update})
-    }
-    handlepageChange=(page_number)=>{
-        this.setState({current_page:page_number})
-    }
-
-    handleListGroup=(genre_name)=>{
-      // console.log(genre_name);
-        var movies_filtered = this.state.movies.filter((movies)=>(
-                movies.genre.name===genre_name.name 
-        ))
-        if(genre_name.name ==="All Genres"){
-            movies_filtered = getMovies();
-        }
-        this.setState({movies:movies_filtered ,current_page:1});
-        
-     }
-
-    render(){
-       const {movies,num_of_movies_per_page,current_page} = this.state;
-          return(
-            <div className ="row">
-                <div className ="col-4">
-                    <ListGroup 
-                        movieGenre = {getGenres} 
-                        handleListGroup={this.handleListGroup}
-                    />
-               </div>
-              
-                <div className ="col-1">
-                     <div>
-                         <MoviesTable 
-                            movies={movies} 
-                            num_of_movies_per_page={num_of_movies_per_page} 
-                            current_page={current_page} 
-                            handleDelete={this.handleDelete}
-                            toggleLike={this.toggleLike} 
-                         />
-                        <Pagination 
-                            movies_count = {this.state.movies_count}
-                            num_of_movies_per_page = {this.state.num_of_movies_per_page}
-                            changePage = {this.handlepageChange} 
-                         />
-                     </div>
-                </div>
-
+  render() {
+    const { movies, num_of_movies_per_page, current_page } = this.state;
+    return (
+      <div className="row">
+        <div className="col-4">
+          <ListGroup
+            movieGenre={getGenres}
+            handleListGroup={this.handleListGroup}
+          />
         </div>
-  
-        );
-    }
+
+        <div className="col-1">
+          <div>
+            <MoviesTable
+              movies={movies}
+              num_of_movies_per_page={num_of_movies_per_page}
+              current_page={current_page}
+              handleDelete={this.handleDelete}
+              toggleLike={this.toggleLike}
+              onSort={this.handleonSort}
+              sortColumn={this.state.sortColumn}
+            />
+            <Pagination
+              movies_count={this.state.movies_count}
+              num_of_movies_per_page={this.state.num_of_movies_per_page}
+              changePage={this.handlepageChange}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 export default Movies;
--- vidly_copy/src/components/moviesTable.jsx
+++ vidly_copy/src/components/moviesTable.jsx
@@ -1,39 +1,65 @@
 import React from "react";
 import Paginate from "../utils/paginate";
 import Like from "./like";
-const MoviesTable = (props)=>{
-    const {movies,num_of_movies_per_page,current_page} = props;
-    const movies_on_current_page = Paginate(movies,current_page,num_of_movies_per_page);
+import _ from "lodash";
+const MoviesTable = props => {
+  const {
+    movies,
+    num_of_movies_per_page,
+    current_page,
+    onSort,
+    sortColumn
+  } = props;
+  const movies_on_current_page = Paginate(
+    movies,
+    current_page,
+    num_of_movies_per_page
+  );
+  //Now adding the sorting here :
+  const sorted = _.orderBy(
+    movies_on_current_page,
+    [sortColumn.path],
+    [sortColumn.order]
+  );
 
-    return(
-      
-        <table className ="table">
-        <thead>
-            <tr>
-                <th>Title</th>
-                <th>Genre</th>
-                <th>Stock</th>
-                <th>DailyRental</th>
-                <th></th>
-                <th></th>
-            </tr>
-        </thead>
-        {movies_on_current_page.map((temp_movie)=>(
-                <tbody key ={temp_movie._id} >
-                    <tr >
-                        <td>{temp_movie.title}</td>
-                        <td>{temp_movie.genre.name}</td>
-                        <td>{temp_movie.numberInStock}</td>
-                        <td>{temp_movie.dailyRentalRate}</td>
-                        <td><Like handleLike={()=>this.toggleLike(temp_movie)} liked_or_not = {temp_movie.liked}></Like></td>
-                        <td>
-                            <button onClick={()=>props.handleDelete(temp_movie)}  type="button" className="btn btn-danger">Delete</button>  
-                        </td>
-                    </tr>
-                </tbody>
-        ))}
-    </table> 
-
-    );
-}
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th onClick={() => onSort("title")}>Title</th>
+          <th onClick={() => onSort("genre.name")}>Genre</th>
+          <th onClick={() => onSort("numberInStock")}>Stock</th>
+          <th onClick={() => onSort("dailyRentalRate")}>DailyRental</th>
+          <th />
+          <th />
+        </tr>
+      </thead>
+      {sorted.map(temp_movie => (
+        <tbody key={temp_movie._id}>
+          <tr>
+            <td>{temp_movie.title}</td>
+            <td>{temp_movie.genre.name}</td>
+            <td>{temp_movie.numberInStock}</td>
+            <td>{temp_movie.dailyRentalRate}</td>
+            <td>
+              <Like
+                handleLike={() => props.toggleLike(temp_movie)}
+                liked_or_not={temp_movie.liked}
+              />
+            </td>
+            <td>
+              <button
+                onClick={() => props.handleDelete(temp_movie)}
+                type="button"
+                className="btn btn-danger"
+              >
+                Delete
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      ))}
+    </table>
+  );
+};
 export default MoviesTable;
