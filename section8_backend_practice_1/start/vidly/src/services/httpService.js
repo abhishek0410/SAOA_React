75ef937ece32afc8ccc235edb14f9bed52c15217
Abhishek <u6411540@anu.edu.au>
2019-02-24 12:28:26 +1100

    Did something, stil confused, leaving it as is,..P.S: We needed to switch to routing and hecnnce had to commit everything in the branch backend

---
+++ section8_backend_practice_1/start/vidly/src/services/httpService.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+import Raven from "raven-js";
+
+axios.interceptors.response.use(null, error => {
+  const expectedError =
+    error.response &&
+    error.response.status >= 400 &&
+    error.response.status < 500;
+  if (!expectedError) {
+    Raven.captureException(error);
+    alert("Unexpected Error occured");
+  }
+  return Promise.reject(error);
+});
+
+const http = {
+  get: axios.get,
+  post: axios.post,
+  put: axios.put,
+  delete: axios.delete
+};
+
+export default http;
